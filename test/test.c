e9739176b7cd83905d6b7138dca62b7ff84ec518
Darren Lin <dlin52@ucsc.edu>
2021-12-20 17:10:48 -0800

    testing counting

--- test/test.c
+++ test/test.c
@@ -4,14 +4,23 @@
 #define HIGHER 300
 
 int main() {
-    float lower;
-    float cel, far;
-    lower = 0;
-    while (lower <= HIGHER){
-        far = lower;
-        cel = (5.0/9.0) * (far-32.0);
-        printf("%3.2f  -   %6.2f\n", far,cel);
-        lower += 20;
-    }
-    return 0;
+    //Finds Celsius from 0 fahr to 300 fahr 
+    // 
+    // float lower;
+    // float cel, far;
+    // lower = 0;
+    // while (lower <= HIGHER){
+    //     far = lower;
+    //     cel = (5.0/9.0) * (far-32.0);
+    //     printf("%3.2f  -   %6.2f\n", far,cel);
+    //     lower += 20;
+    // }
+
+    int c, nl;
+    nl = 0;
+    while ((c = getchar()) != EOF)
+        if (c == '\n')
+        ++nl;
+    printf("%d\n", nl);
+  
 }

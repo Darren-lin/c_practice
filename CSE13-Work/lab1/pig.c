af9a6bcafbe1b81ed3f1125ba38517f4adbbe937
Darren Lin <dlin52@ucsc.edu>
2022-02-07 16:13:10 -0800

    # of players and zeroing scores

--- CSE13-Work/lab1/pig.c
+++ CSE13-Work/lab1/pig.c
@@ -1,1 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
 
+int main() {
+
+    //prompting user for # of players
+    int input = 0;
+    scanf("%d", &input);
+    if(input > 10 || input < 2){
+        fprintf(stderr,"Invalid number of players. Using 2 instead.\n");
+    }
+
+    //declaring and zeroing out scores array
+    int scores[input];
+    for(int i = 0; i < input; i++){
+        scores[i] = 0;
+    }
+
+
+    return 0;
+}

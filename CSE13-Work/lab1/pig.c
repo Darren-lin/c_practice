75da75b304108c06050e8d3cb7d689c44ac8aab7
Darren Lin <dlin52@ucsc.edu>
2022-02-25 01:22:01 -0800

    finished

--- CSE13-Work/lab1/pig.c
+++ CSE13-Work/lab1/pig.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <inttypes.h>
 #include "names.h"
 
-#define SEED 2022;
+#define SEED 2022
 
 typedef enum
 {
@@ -17,7 +18,7 @@ typedef enum
 
 const Position pig[7] = {SIDE, SIDE, RAZORBACK, TROTTER, SNOUTER, JOWLER, JOWLER};
 const int points[7] = {0, 0, 10, 10, 15, 5, 5};
-const char *msg[7] = {"pig lands on side", "pig lands on side", "pig lands on back", "pig lands upright", "pig lands snout", "pig lands on ear", "pig lands on ear"};
+const char *msg[7] = {" pig lands on side", " pig lands on side", " pig lands on back", " pig lands upright", " pig lands on snout", " pig lands on ear", " pig lands on ear"};
 
 int main()
 {
@@ -30,6 +31,8 @@ int main()
         fprintf(stderr, "Invalid number of players. Using 2 instead.\n");
         numplayers = 2;
     }
+
+    // creating and names and score array
     int scores[numplayers];
     const char *players[numplayers];
     for (int i = 0; i < numplayers; i++)
@@ -39,33 +42,33 @@ int main()
     }
 
     // prompting user for seed
-    long int seed;
+    int64_t seed;
     printf("Random seed: ");
-    if (scanf("%ld", &seed) == EOF || seed < 0)
+    if (scanf("%" PRId64, &seed) == EOF || seed < 0)
     {
         fprintf(stderr, "Invalid random seed. Using 2022 instead.\n");
         seed = SEED;
     }
     srandom(seed);
 
+    // play loop
     Position action;
     int player = 0;
     bool play = true;
     int temp;
-    // play loop
     while (play)
     {
         if (player == numplayers)
         {
             player = 0;
         }
-        printf("%s rolls the pig... ", names[player]);
+        printf("%s rolls the pig...", names[player]);
         do
         {
             temp = random() % 7;
             action = pig[temp];
             scores[player] += points[temp];
-            printf("%s ", msg[temp]);
+            printf("%s", msg[temp]);
         } while (action != SIDE && scores[player] < 100);
         printf("\n");
         if (scores[player] >= 100)
@@ -77,6 +80,7 @@ int main()
             player += 1;
         }
     }
-    printf("%s wins with %d points!", names[player], scores[player]);
-    return 0;
+
+    printf("%s wins with %d points!\n", names[player], scores[player]);
+    return EXIT_SUCCESS;
 }

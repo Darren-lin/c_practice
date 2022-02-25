543786258b6f9c1dbd5c03bcd3825a814e8d4bcf
Darren Lin <dlin52@ucsc.edu>
2022-02-24 22:04:39 -0800

    adding play

--- CSE13-Work/lab1/pig.c
+++ CSE13-Work/lab1/pig.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <limits.h>
 #include <string.h>
+#include <stdbool.h>
 #include "names.h"
 
 #define SEED 2022;
@@ -15,7 +16,8 @@ typedef enum
     JOWLER
 } Position;
 const Position pig[7] = {SIDE, SIDE, RAZORBACK, TROTTER, SNOUTER, JOWLER, JOWLER};
-
+const int points[7] = {0, 0, 10, 10, 15, 5};
+const char *msg[7] = {"pig lands on side", "pig lands on side", "pig lands on back", "pig lands upright", "pig lands on ear", "pig lands on ear"};
 int main()
 {
     // prompting user for # of players
@@ -27,16 +29,11 @@ int main()
         fprintf(stderr, "Invalid number of players. Using 2 instead.\n");
         numplayers = 2;
     }
+    int scores[numplayers];
     const char *players[numplayers];
     for (int i = 0; i < numplayers; i++)
     {
         players[i] = names[i];
-    }
-
-    // declaring and zeroing out scores array
-    int scores[numplayers];
-    for (int i = 0; i < numplayers; i++)
-    {
         scores[i] = 0;
     }
 
@@ -50,10 +47,37 @@ int main()
     }
     srandom(seed);
 
-    // attempting to get action
-    for (int i = 0; i < 10; i++)
+    Position action = 6;
+    int player = 0;
+    bool play = true;
+    while (play)
     {
-        printf("%d\n", pig[random() % 7]);
+        if (player == 8)
+        {
+            player = 0;
+        }
+        do
+        {
+            action = pig[random() % 7];
+        } while ((action != SIDE) && scores[player] < 100);
+        {
+            if (action == RAZORBACK)
+            {
+                scores[player] += 10;
+            }
+            else if (action == TROTTER)
+            {
+                scores[player] += 10;
+            }
+            else if (action == SNOUTER)
+            {
+                scores[player] += 15;
+            }
+            else if (action == JOWLER)
+            {
+                scores[player] += 5;
+            }
+        }
+        player += 1;
+        return 0;
     }
-    return 0;
-}
